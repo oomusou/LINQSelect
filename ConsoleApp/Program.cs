d6308e816c20ed6898b47382f1736b6d8ff63895
oomusou <oomusou@gmail.com>
2018-09-25 18:36:42 +0800

    Refactor to Extension Method

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,23 +4,20 @@ using System.Linq;
 
 namespace ConsoleApp
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            var data = Enumerable.Range(1, 3);
-
-            var result = MyMap(data, Double);
-
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
-            
+            Enumerable
+                .Range(1, 3)
+                .MyMap(Double)
+                .ToList()
+                .ForEach(Console.WriteLine);
+                
             int Double(int x) => x * 2;
         }
         
-        private static IEnumerable<R> MyMap<T, R>(IEnumerable<T> data, Func<T, R> func)
+        private static IEnumerable<R> MyMap<T, R>(this IEnumerable<T> data, Func<T, R> func)
         {
             foreach (var item in data)
             {
