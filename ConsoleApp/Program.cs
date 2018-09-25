c951b18b55751b64a2894f370608139df44adbb1
oomusou <oomusou@gmail.com>
2018-09-25 18:26:50 +0800

    Refactor to HOF

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,17 +10,26 @@ namespace ConsoleApp
         {
             var data = Enumerable.Range(1, 3);
 
-            var result = new List<int>();
-
-            foreach (var item in data)
-            {
-                result.Add(item * 2);
-            }
+            var result = MyMap(data, Double);
 
             foreach (var item in result)
             {
                 Console.WriteLine(item);
             }
+            
+            int Double(int x) => x * 2;
+        }
+        
+        private static IEnumerable<int> MyMap(IEnumerable<int> data, Func<int, int> func)
+        {
+            var result = new List<int>();
+
+            foreach (var item in data)
+            {
+                result.Add(func(item));
+            }
+
+            return result;
         }
     }
 }
